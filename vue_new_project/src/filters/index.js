1ee01cd4ad90970f0b0f53d6e30324c06f95cef1
xiangguoqing <xiangguoqing@chinamobo.com>
2018-01-04 00:43:30 +0800

    add vue_new_project
    Signed-off-by: xiangguoqing <xiangguoqing@chinamobo.com>

---
+++ vue_new_project/src/filters/index.js
@@ -0,0 +1,11 @@
+import {normalTime} from './timeFormat';
+
+/* webpack2 cannot use both import and module.exports*/
+/* https://github.com/webpack/webpack/issues/3997 */
+/* module.exports={
+	normalTime
+}; */
+
+export default {
+	normalTime
+};
