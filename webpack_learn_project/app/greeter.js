6e11c2a014b3c575e9de05dc0553ab7ad56045a4
xiangguoqing <xiangguoqing@chinamobo.com>
2018-01-23 18:40:48 +0800

    更新
    Signed-off-by: xiangguoqing <xiangguoqing@chinamobo.com>

--- webpack_learn_project/app/greeter.js
+++ webpack_learn_project/app/greeter.js
@@ -1,5 +1,21 @@
+/* var config = require("./config.json");
 module.exports = function(){
     var greet = document.createElement("div");
-    greet.textContent = "Hi there and greetings!";
+    greet.textContent = config.greetText;
     return greet;
+} */
+
+import React, { Component } from "react";
+import { config } from "./config.json";
+
+class Greeter extends Component {
+    render(){
+        return (
+            <div>
+                {config.greetText}
+            </div>
+        );
+    }
 }
+
+export default Greeter
--- webpack_learn_project/webpack.config.js
+++ webpack_learn_project/webpack.config.js
@@ -1,10 +1,50 @@
 module.exports = {
-    devtool:"eval-source-map",
-    entry: __dirname + "/app/main.js",
+    devtool: "source-map",
+    entry: __dirname + "/app/main.jsx",
     output: {
-        path:__dirname + "/public",
-        filename:"bundle.js"
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: "./public",
+        historyApiFallback: true,
+        inline: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /(\.jsx|\.js)$/,  // .jsx为react文件
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["react", "env"]
+                    }
+                },
+                /* loader:"babel-loader",
+                query: {
+                    presets: ['env']
+                }, */
+                exclude: /node_modules/
+            }
+        ]
+        // rules 和 loaders 功能相同
+        /* ,
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "env"
+                        ]
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ] */
+
     }
 
 }
-console.log(__dirname, "!");
+
